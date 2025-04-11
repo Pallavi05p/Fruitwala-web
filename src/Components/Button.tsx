d3a75c8c092f19274a55d1917ecbc7bf95d95da2
Pallavi05p <pallavidhanwe123@gmail.com>
2025-04-11 16:56:21 +0530

    chnas

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,7 +1,8 @@
 import { Slot } from "@radix-ui/react-slot";
 import * as React from "react";
 import { type VariantProps, cva } from "class-variance-authority";
-import { cn } from "../Lib/utils.js"; 
+import { cn } from "../lib/utils.js";
+// import { cn } from "../Lib/utils.js"; 
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
